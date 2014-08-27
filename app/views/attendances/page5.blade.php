0f72dc86f772ab46d1d892db5815e59ada60411c
sudebkumar <sudeb.phy137@gmail.com>
2014-08-27 17:14:15 +0600

    page5.blade.php class_project commit

---
+++ app/views/attendances/page5.blade.php
@@ -0,0 +1,55 @@
+@extends('layouts.default')
+
+@section('main')
+
+<div id="content">
+    <div id="content-header" class="mini">
+        <h1>By Training Type</h1>
+    </div>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-xs-12 center" style="text-align: center;">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4 class="text-center">Performance Report</h4>
+                    </div>
+                    <div class="panel-body">
+                        <div class="list-group">
+                            <a href="#" class="list-group-item ">
+
+                                <p class="list-group-item-text">
+                                    By Training Type
+                                </p>
+                            </a>
+                            <a href="#" class="list-group-item">
+
+                                <p class="list-group-item-text">
+                                    By Specific Track
+                                </p>
+                            </a>
+                            <a href="#" class="list-group-item">
+
+                                <p class="list-group-item-text">
+                                    By Month
+                                </p>
+                            </a>
+                        </div>
+                        <a href="" class="btn btn-info col-lg-offset-7 col-lg-5">
+                            Next
+                            <b class="glyphicon glyphicon-arrow-right"></b>
+                        </a>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        <br />
+
+
+    </div>
+
+
+
+</div>
+
+@stop
